41e791afc86e803e22af8a116f87b5761746f924
Urban ISHIMWE <urbanishimwe1@gmail.com>
2025-07-21 11:43:17 +0200

    docs for configuration file

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -28,11 +28,13 @@ export const ROUTES: EachRoute[] = [
     items: [
       { title: "Authentication", href: "/authentication" },
       { title: "Organizations", href: "/organizations" },
+       { title: "Configuration", href: "/configuration" },
       { title: "Apps", href: "/apps" },
       { title: "Setup", href: "/setup" },
       { title: "Deploy", href: "/deploy" },
       { title: "Builder", href: "/builder" },
       { title: "Machines", href: "/machines" },
+      { title: "Secrets", href: "/secrets" },
     ]
   },
   {
